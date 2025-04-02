45ad1ffde148fd16a9e5de1ee2c881e7783b34a2
Connor DeLuca <cojode6@gmail.com>
2025-04-02 09:33:04 -0400

    Update

--- main.py
+++ main.py
@@ -5,11 +5,21 @@ score = {'wins':0,'losses':0,'ties':0}
 choices = ['rock','paper','scissors']
 
 wantsToPlayAgain = ''
-# The user can exit by typing nothing
+
+def win():
+    global wantsToPlayAgain
+    print('You win!')
+    score['wins'] = score['wins'] + 1
+    wantsToPlayAgain = input('Would you like to play again? Type yes or no: ').lower()
+
+# The user can exit by typing no at the end
 while wantsToPlayAgain != 'no':
-    useraction = input('Enter rock, paper, or scissors to play. Type nothing to exit the program.').lower()
+    useraction = input('Enter rock, paper, or scissors to play: ').lower()
     computeraction = choices[random.randint(0,2)]
     print(computeraction)
 
     if useraction == 'rock':
-        if computeraction 
+        if computeraction == 'scissors':
+            win()
+
+    print('The current score is: \n ' + str(score))
