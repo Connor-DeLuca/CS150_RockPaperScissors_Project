f7945fd4e3eb6b697714019e33eb6963d12a8afc
Connor DeLuca <cojode6@gmail.com>
2025-04-02 09:24:43 -0400

    Fix

--- main.py
+++ main.py
@@ -6,7 +6,10 @@ choices = ['rock','paper','scissors']
 
 wantsToPlayAgain = ''
 # The user can exit by typing nothing
-while wantsToPlayAgain != 'yes':
+while wantsToPlayAgain != 'no':
     useraction = input('Enter rock, paper, or scissors to play. Type nothing to exit the program.').lower()
     computeraction = choices[random.randint(0,2)]
     print(computeraction)
+
+    if useraction == 'rock':
+        if computeraction 
