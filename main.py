fca4bae1b1aad811d49eca1cb6cf1f4b6ce6ced7
Connor DeLuca <cojode6@gmail.com>
2025-04-02 09:15:48 -0400

    makes input lowercase

--- main.py
+++ main.py
@@ -3,4 +3,5 @@ score = 0
 useraction = ''
 # The user can exit by typing nothing
 while useraction != '':
-    useraction = input('Enter rock, paper, or scissors to play. Type nothing to exit the program.')
+    useraction = input('Enter rock, paper, or scissors to play. Type nothing to exit the program.').lower()
+    
