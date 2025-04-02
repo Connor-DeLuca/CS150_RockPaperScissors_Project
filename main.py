1290117d7d614a56f0ecea80e54737102b037f71
Connor DeLuca <cojode6@gmail.com>
2025-04-02 09:42:58 -0400

    functions added

--- main.py
+++ main.py
@@ -10,7 +10,26 @@ def win():
     global wantsToPlayAgain
     print('You win!')
     score['wins'] = score['wins'] + 1
+    # Prints the score
+    print('The current score is: \n ' + str(score))
     wantsToPlayAgain = input('Would you like to play again? Type yes or no: ').lower()
+
+def loss():
+    global wantsToPlayAgain
+    print('You lose!')
+    score['losses'] = score['losses'] + 1
+    # Prints the score
+    print('The current score is: \n ' + str(score))
+    wantsToPlayAgain = input('Would you like to play again? Type yes or no: ').lower()
+
+def tie():
+    global wantsToPlayAgain
+    print('You tied!')
+    score['ties'] = score['ties'] + 1
+    # Prints the score
+    print('The current score is: \n ' + str(score))
+    wantsToPlayAgain = input('Would you like to play again? Type yes or no: ').lower()
+
 
 # The user can exit by typing no at the end
 while wantsToPlayAgain != 'no':
@@ -21,5 +40,24 @@ while wantsToPlayAgain != 'no':
     if useraction == 'rock':
         if computeraction == 'scissors':
             win()
-
-    print('The current score is: \n ' + str(score))
+        if computeraction == 'paper':
+            loss()
+        if computeraction == 'rock':
+            tie()
+    elif useraction == 'paper':
+        if computeraction == 'rock':
+            win()
+        if computeraction == 'scissors':
+            loss()
+        if computeraction == 'paper':
+            tie()
+    elif useraction == 'scissors':
+        if computeraction == 'paper':
+            win()
+        if computeraction == 'rock':
+            loss()
+        if computeraction == 'scissors':
+            tie()
+    else:
+        print('Not a valid choice. Please try again.')
+        # Loop restarts
