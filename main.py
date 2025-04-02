89223a06d5731a524ee4fa7f9987582c25e929ef
Connor DeLuca <cojode6@gmail.com>
2025-04-02 09:18:30 -0400

    score

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-score = 0
+score = {'wins':0,'losses':0,'ties':0}
 
 useraction = ''
 # The user can exit by typing nothing
 while useraction != '':
     useraction = input('Enter rock, paper, or scissors to play. Type nothing to exit the program.').lower()
-    
