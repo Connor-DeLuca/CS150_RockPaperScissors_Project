4a865f7027884704594b4cf51b27713c7846baf1
Connor DeLuca <cojode6@gmail.com>
2025-04-02 09:14:37 -0400

    commit

---
+++ main.py
@@ -0,0 +1,6 @@
+score = 0
+
+useraction = ''
+# The user can exit by typing nothing
+while useraction != '':
+    useraction = input('Enter rock, paper, or scissors to play. Type nothing to exit the program.')
